1d6f6d74a5a9a6ea33477cccf6579d2a8a529775
Fernando Batista <fernandohsbatista@gmail.com>
2021-05-18 07:36:42 -0300

    Initial commit

---
+++ bin/eskola.dart
@@ -0,0 +1,5 @@
+import 'package:eskola/eskola.dart' as eskola;
+
+void main(List<String> arguments) {
+  print('Hello world: ${eskola.calculate()}!');
+}
---
+++ test/eskola_test.dart
@@ -0,0 +1,8 @@
+import 'package:eskola/eskola.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
